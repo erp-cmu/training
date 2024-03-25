da64c319bb1753ba63e8ee3c75479fdfb5c326b9
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-03-26 00:22:28 +0700

    Update

---
+++ scripts/install_4_prod.sh
@@ -0,0 +1,14 @@
+# Description: Install Frappe and ERPNext on Ubuntu 22.04-jammy
+# Part 4: Setup for production.  Make sure that the terminal is **not** running `bench start`
+
+# Set up production
+sudo bench setup production $USER
+
+# Replace user in nginx.conf
+sudo sed -i "s/user www-data/user $USER/g" /etc/nginx/nginx.conf
+
+# Supervisor
+sudo ln -sf $HOME/erp/config/supervisor.conf /etc/supervisor/conf.d/frappe-bench.conf
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl status
